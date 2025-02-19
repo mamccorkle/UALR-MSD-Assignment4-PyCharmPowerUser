8286d28b7aa6107accec087eb7719f695a1ea228
Mark McCorkle <mamccorkle@ualr.edu>
2025-02-19 15:00:53 -0600

    Step 5:    * FIX: Used debugging tools Step 5 Complete
    Step 6:
       * FIX: Added docstrings to each of the functions
    Step 6 Complete

--- README.md
+++ README.md
@@ -66,7 +66,7 @@ t()
 ✅ **Reformat the code using auto-formatting** → Use **Code > Reformat Code** (Ctrl + Alt + L)<br>
 ✅ **Run Code Analysis & Fix Warnings** → Use **Inspect Code** (Code > Inspect Code)<br>
 ✅ **Extract some code from t() and make it cleaner**→ Use **Refactor > Extract Method**(Ctrl + Alt + M)<br>
-❌ **Add a breakpoint & step through execution** → Use **Debugging Tools** (Ctrl + F8)<br>
-❌ **Create a docstring for each function** → Use **PyCharm's Generate Docstring** (Alt + Enter) or Use **AI** (Ctrl + \)<br>
+✅ **Add a breakpoint & step through execution** → Use **Debugging Tools** (Ctrl + F8)<br>
+✅ **Create a docstring for each function** → Use **PyCharm's Generate Docstring** (Alt + Enter) or Use **AI** (Ctrl + \)<br>
 ❌ **Run the script with Code Coverage enabled** (Run > Run with Coverage)<br>
 ❌ **Use Git to track changes & commit the final version** (Ctrl + K to commit)<br>
--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 def print_grid(board):
+    """
+
+        This function prints the grid out to the console.
+
+    :param board: 2D Array of the board layout
+    """
     for row in board:
         print(" | ".join(row))
         print("-" * 5)
 
 
 def check_winner(board, players):
+    """
+
+        This function checks to see if a winner has been declared
+
+    :param board: 2D Array of the board layout
+    :param players: Array of the X and Os
+    :return: Boolean value representing a winning value
+    """
     for i in range(3):
         if all(board[i][j] == players for j in range(3)) or all(board[j][i] == players for j in range(3)):
             return True
@@ -14,10 +28,23 @@ def check_winner(board, players):
 
 
 def tie(board):
+    """
+
+        This function checks to see if a tie has been reached
+
+    :param board: 2D Array of the board layout
+    :return: Boolean value representing a tie
+    """
     return all(col != " " for row in board for col in row)
 
 
 def tictactoe():
+    """
+
+        This is the main function of the program
+
+    :return: Boolean value representing the successful completion of the program or not.
+    """
     board = [[" " for _ in range(3)] for _ in range(3)]
     players = ["X", "O"]
     print("Tic-Tac-Toe Game")
@@ -45,6 +72,13 @@ def tictactoe():
 
 
 def get_input(player):
+    """
+
+        This function gets the user input for where the user wants to place their character
+
+    :param player: Current player in the selection
+    :return: position user selected
+    """
     row, col = map(int, input(f"P {player}, row col (0-2): ").split())
     return col, row
 
